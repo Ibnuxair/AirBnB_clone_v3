03f80c1ea4c9a3c4b520d81bf6ceaa78cfcc35e3
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-09 13:09:38 +0000

    AirBnB

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,15 +10,18 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown_storage(exception):
     """Close the storage when the app context ends"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """Returns a JSON-formatted 404 status code response."""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
