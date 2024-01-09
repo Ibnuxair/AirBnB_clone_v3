38644faf4a00b007634cd7c6d028aa0855d0d9be
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-09 14:43:49 +0000

    AirBnB

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 Get the status of the API.
 """
 
+from flask_cors import CORS
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
@@ -11,6 +12,8 @@ import os
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
+
+CORS(app, resources={r"/api/*": {"origins": "http://0.0.0.0"}})
 
 
 @app.teardown_appcontext
